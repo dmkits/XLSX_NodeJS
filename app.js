3b62af20f9c9fe2a22c03d1baccf0f2633756d5e
ianagez <ianagez88@gmail.com>
2017-10-03 13:52:32 +0300

    Download XLSX FILE whith xhrRequest

--- app.js
+++ app.js
@@ -4,14 +4,25 @@ var express = require('express');
 var app = express();
 var path=require ('path');
 var XLSX=require('xlsx');
-
+var bodyParser = require('body-parser');
 var port=8183;
+
 app.use('/',express.static('public'));
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({limit: '5mb'}));
+app.use(bodyParser.text());
 
 var ConfigurationError, DBConnectError;
 
-
 app.get('/', function (req, res) {
+    //var fname = path.join(__dirname, '/out.xlsx');
+    res.sendFile(path.join(__dirname, '/load_file.html'));
+});
+
+
+app.post('/get_file', function (req, res) {
+
+    console.log("req.body=",req.body);
 
     var data =JSON.parse(fs.readFileSync("/home/ianagez/IdeaProjects/chat/testInfoTable.json"));
     var fname = path.join(__dirname, '/out.xlsx');
@@ -19,7 +30,7 @@ app.get('/', function (req, res) {
     fs.open(fname, 'w', function (err) {
         if (err){
             console.log("err=", err);
-            res.end(); /////
+            res.end();
             return;
         }
         var wb = XLSX.readFile(fname);
@@ -40,7 +51,7 @@ app.get('/', function (req, res) {
         res.sendFile(fname, options, function(err){
             if(err){
                 console.log("err=",err);
-                res.end(); /////
+                res.end(); ///
                 return;
             }
             fs.unlinkSync(fname); console.log("fname unlinkSync=", fname);
